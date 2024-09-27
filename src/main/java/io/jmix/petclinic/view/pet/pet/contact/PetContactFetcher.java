7a0da30352709fffa99db2aebc53a26d45b3b3b8
Mario David <mario@die-davids.de>
2024-09-27 19:01:26 +0200

    make contact fetcher more functional

--- src/main/java/io/jmix/petclinic/view/pet/pet/contact/PetContactFetcher.java
+++ src/main/java/io/jmix/petclinic/view/pet/pet/contact/PetContactFetcher.java
@@ -1,72 +1,57 @@
 package io.jmix.petclinic.view.pet.pet.contact;
 
-import io.jmix.core.DataManager;
-import io.jmix.core.Id;
 import io.jmix.core.Messages;
 import io.jmix.petclinic.entity.owner.Owner;
 import io.jmix.petclinic.entity.pet.Pet;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.util.Optional;
-
-import static org.springframework.util.StringUtils.*;
+import java.util.stream.Stream;
 
 
 // tag::fetch-contact-logic[]
 @Component
 public class PetContactFetcher {
 
-    private final DataManager dataManager;
     private final Messages messages;
 
-    public PetContactFetcher(DataManager dataManager, Messages messages) {
-        this.dataManager = dataManager;
+    public PetContactFetcher(Messages messages) {
         this.messages = messages;
     }
 
     public Optional<Contact> findContact(Pet pet) {
-
-        Optional<Owner> petOwner = loadOwnerFor(pet);
-
-        if (petOwner.isPresent()) {
-
-            Owner owner = petOwner.get();
-            String telephone = owner.getTelephone();
-            String email = owner.getEmail();
-
-            if (hasText(telephone)) {
-                return createContact(telephone, ContactType.TELEPHONE);
-            } else if (hasText(email)) {
-                return createContact(email, ContactType.EMAIL);
-            } else {
-                String address = formatOwnerAddress(owner);
-                if (hasText(address)) {
-                    return createContact(address, ContactType.ADDRESS);
-                } else {
-                    return Optional.empty();
-                }
-            }
-        } else {
-            return Optional.empty();
-        }
+        return Optional.ofNullable(pet.getOwner())
+                .flatMap(owner ->
+                        Stream.of(
+                                        contactInfo(owner.getTelephone(), ContactType.TELEPHONE),
+                                        contactInfo(owner.getEmail(), ContactType.EMAIL),
+                                        contactInfo(ownerAddress(owner), ContactType.ADDRESS)
+                                )
+                                .filter(Optional::isPresent)
+                                .findFirst()
+                                .orElse(Optional.empty())
+                );
     }
 
-    private Optional<Contact> createContact(String contactValue, ContactType contactType) {
+    private Optional<Contact> contactInfo(String contactInfo, ContactType contactType) {
+        return Optional.ofNullable(contactInfo)
+                .filter(StringUtils::hasText)
+                .map(info -> createContact(info, contactType));
+    }
+
+    private Contact createContact(String contactValue, ContactType contactType) {
         Contact contact = new Contact();
         contact.setValue(contactValue);
         contact.setType(contactType);
-        return Optional.of(contact);
+        return contact;
     }
 
-    private String formatOwnerAddress(Owner owner) {
-        return messages.formatMessage(this.getClass(), "ownerAddressFormat", owner.getFirstName(), owner.getLastName(), owner.getAddress(), owner.getCity());
-    }
-
-    private Optional<Owner> loadOwnerFor(Pet pet) {
-        if (pet.getOwner() == null) {
-            return Optional.empty();
-        }
-        return dataManager.load(Id.of(pet.getOwner())).optional();
+    private String ownerAddress(Owner owner) {
+        return messages.formatMessage(
+                this.getClass(), "ownerAddressFormat",
+                owner.getFirstName(), owner.getLastName(), owner.getAddress(), owner.getCity()
+        );
     }
 }
 // end::fetch-contact-logic[]
